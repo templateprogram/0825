52e55c9b07cb015109edede0d6b2518400b06496
RyanLuPersonal <stones4590@gmail.com>
2023-08-21 11:13:59 +0800

    adding img_attributes in normal single img

--- database/migrations/example/2023_07_27_153328_create_normal_table.php
+++ database/migrations/example/2023_07_27_153328_create_normal_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->softDeletes();
+            // $table->longText('url')->nullable();
+            // $table->json('img_attributes')->nullable();
             $table->string('name');
             $table->longText('back_body')->nullable();
             $table->boolean('active')->default(1);
